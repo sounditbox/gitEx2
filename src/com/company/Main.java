c217975a24795438ac7afecec6d56ab32ae2c7c1
Ruslan <mutygullin@yandexlyceum.ru>
2022-06-05 14:02:17 +0300

    deleted Blabla.java, moved comments to Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your text here
     }
 }
+
+
+// untracked - красные - не отслеживаемые файлы. (git add .) чтобы добавить их все
+// ignored - жёлтые - игнорируемые. (список лежит в .gitgnore)
+
+// new - зелёные - новые
+// changed - лазурные - изменённые
+// deleted - удалённые
