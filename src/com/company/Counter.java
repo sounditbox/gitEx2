83cdea4942f9ab36d6af7f74f35f267cb3f9bb2e
Ruslan <mutygullin@yandexlyceum.ru>
2022-06-05 14:25:27 +0300

    added Counter, changed Main

---
+++ src/com/company/Counter.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Counter {
+    private int count;
+    public Counter(){
+        count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your text here
+	    Counter c = new Counter();
+        System.out.println(c.getCount());
     }
 }
 
