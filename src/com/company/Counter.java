c7644ad61cc70083c59a22bb27941f66a90a6a4f
Ruslan <mutygullin@yandexlyceum.ru>
2022-06-05 15:02:31 +0300

    added inc and dec to Counter

--- src/com/company/Counter.java
+++ src/com/company/Counter.java
@@ -2,11 +2,20 @@ package com.company;
 
 public class Counter {
     private int count;
-    public Counter(){
+
+    public Counter() {
         count = 0;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int inc() {
+        return ++count;
+    }
+
+    public int dec() {
+        return --count;
     }
 }
